86bd89c937f3d898b00cf1bd6aed3339a7eace65
Meidon <steffen.star@hotmail.com>
2016-04-01 21:47:05 +0200

    Selection Fix
    Now you can actually select stuff with a selectionCircle and everything
    now onto marker placement and updating it to selected units

--- Testing/Assets/Scripts/General/Unit.cs
+++ Testing/Assets/Scripts/General/Unit.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Unit : MonoBehaviour {
 
     bool isSelected = false;
+    public GameObject selectionCircle;
 
-
-
-	void FixedUpdate () {
-	
-	}
 }
--- Testing/Assets/Scripts/General/UnitSelection.cs
+++ Testing/Assets/Scripts/General/UnitSelection.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 public class UnitSelection : MonoBehaviour {
 
     bool isSelecting = false;
     Vector3 mousePosition1;
+
+    public GameObject selectionCirclePrefab;
 
     void FixedUpdate()
     {
@@ -12,11 +18,65 @@ public class UnitSelection : MonoBehaviour {
         {
             isSelecting = true;
             mousePosition1 = Input.mousePosition;
+
+            foreach(var selectableObject in FindObjectsOfType<Unit>())
+            {
+                if(selectableObject.selectionCircle != null)
+                {
+                    Destroy(selectableObject.selectionCircle.gameObject);
+                    selectableObject.selectionCircle = null;
+                }
+            }
         }
 
         if(Input.GetMouseButtonUp(0))
         {
+
+            var selectedObjects = new List<Unit>();
+            foreach(var selectableObject in FindObjectsOfType<Unit>())
+            {
+                if(IsWithinSelectionBounds(selectableObject.gameObject))
+                {
+                    selectedObjects.Add(selectableObject);
+                }
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("Selecting [{0}] Units", selectedObjects.Count));
+            foreach(var selectedObject in selectedObjects)
+            {
+                sb.AppendLine(" -> " + selectedObject.gameObject.name);
+            }
+
+            Debug.Log(sb.ToString());
+
             isSelecting = false;
+
+
+        }
+
+        if(isSelecting)
+        {
+            foreach(var selectableObject in FindObjectsOfType<Unit>())
+            {
+                if(IsWithinSelectionBounds(selectableObject.gameObject))
+                {
+                    if(selectableObject.selectionCircle == null)
+                    {
+                        selectableObject.selectionCircle = Instantiate(selectionCirclePrefab);
+                        selectableObject.selectionCircle.transform.SetParent(selectableObject.transform, false);
+                        selectableObject.selectionCircle.transform.eulerAngles = new Vector3(90, 0, 0);
+                    }
+                }
+                else
+                {
+                    if(selectableObject.selectionCircle != null)
+                    {
+                        Destroy(selectableObject.selectionCircle.gameObject);
+                        selectableObject.selectionCircle = null;
+                    }
+                }
+            }
         }
 
     }
