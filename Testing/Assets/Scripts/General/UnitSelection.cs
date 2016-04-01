00e181f190858aa8132e1a245d72d18d1afc311d
Meidon <steffen.star@hotmail.com>
2016-04-01 20:30:03 +0200

    A little Update
    - adds beta selection
    - changes some movement behaviors
    - adds a toolbox for quick access

--- Testing/Assets/Scripts/General/UnitSelection.cs
+++ Testing/Assets/Scripts/General/UnitSelection.cs
@@ -3,79 +3,45 @@ using System.Collections;
 
 public class UnitSelection : MonoBehaviour {
 
-    private Vector2 mouseButton1DownP;
-    private Vector2 mouseButton1UpP;
-    private Vector2 mouseButton2DownPoint;
-    private Vector2 mouseButton2UpPoint;
+    bool isSelecting = false;
+    Vector3 mousePosition1;
 
-
-    private Vector3 nouseButton1DownTerrainHitPoint;
-    private Vector3 selectionPointStart;
-    private Vector3 selectionPointEnd;
-
-    private bool mouseLeftDrag = false;
-
-    private LayerMask terrainLayerMask = 1 << 8;
-
-    private float raycastLength = 200.0f;
-
-    public Texture selectionTexture;
-
-    private int mouseButtonReleaseBlurRange = 20;
-
-
-    private void OnGUI()
+    void FixedUpdate()
     {
-        if (mouseLeftDrag)
+        if(Input.GetMouseButtonDown(0))
         {
-            float w = mouseButton1UpP.x - mouseButton1DownP.x;
-            float h = (Screen.height - mouseButton1UpP.y) - (Screen.height - mouseButton1DownP.y);
-            Rect rect = new Rect(mouseButton1DownP.x, Screen.height - mouseButton1DownP.y, w, h);
-            GUI.DrawTexture(rect, selectionTexture, ScaleMode.StretchToFill, true);
+            isSelecting = true;
+            mousePosition1 = Input.mousePosition;
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            isSelecting = false;
         }
 
     }
 
-   
-
-    private void Start()
+    void OnGUI()
     {
-        this.transform.position = Vector3.zero;
-    }
-	
-	private void FixedUpdate () {
-
-        if (Input.GetButtonDown("Fire1"))
+        if(isSelecting)
         {
-           
-
+            //Create the Rectangle
+            var rect = Tools.GetScreenRect(mousePosition1, Input.mousePosition);
+            Tools.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+            Tools.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
         }
-
-
-
-
-        //var mTarget = Input.mousePosition;
-
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    this.transform.position = mTarget;
-
-        //}
-
     }
 
-    private void Mouse1DownDrag(Vector2 screenPos)
+    public bool IsWithinSelectionBounds(GameObject go)
     {
-        if (screenPos != mouseButton1DownP)
-        {
-            mouseLeftDrag = true;
+        if (!isSelecting)
+            return false;
 
-            mouseButton1UpP = screenPos;
+        var camera = Camera.main;
+        var viewportBounds = Tools.GetViewportBounds(camera, mousePosition1, Input.mousePosition);
 
-            RaycastHit hit;
-
-
-        }
-
+        return viewportBounds.Contains(camera.WorldToViewportPoint(go.transform.position));
     }
+
+
 }
